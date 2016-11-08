177d58ac6428dd3f9c970cd70ef0b03f94d85706
Jon Rohan <rohan@github.com>
2016-11-08 07:28:40 -0500

    Adding lint test and update for new octicons and utilities

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,22 +18,22 @@ module.exports = function(grunt) {
         }
       }
     }
-  });
+  })
 
-  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-sass')
 
   grunt.registerTask('default', ["sass", "build"])
 
   grunt.registerTask('build', '', function() {
-    var pattern = "tmp/*.css";
+    var pattern = "tmp/*.css"
 
-    var files = glob.sync(pattern);
+    var files = glob.sync(pattern)
 
-    var cleanSelector = function(selector) {
+    function cleanSelector(selector) {
       return selector.replace(/[\s\.]/g,"").replace(/::?\S+$/,"")
     }
 
-    var buildCompleter = function(selector, comment, filename, section) {
+    function buildCompleter(selector, comment, filename, section) {
       var comp = {
         text: cleanSelector(selector),
         type: "class",
@@ -47,13 +47,13 @@ module.exports = function(grunt) {
     }
 
     var readClasses = postcss.plugin('readClasses', function() {
-      return function (css, opts) {
+      return function(css, opts) {
         var comment = null
         var section = null
         var completions = []
         var compIndex = {}
         var filename = opts.opts.file.replace(/(tmp\/|\.css)/g, "")
-        var trackable = function(selector) {
+        function trackable(selector) {
           if (compIndex[cleanSelector(selector)]) {
             return false
           }
@@ -61,7 +61,7 @@ module.exports = function(grunt) {
           if (periods == null) {
             return false
           }
-          if (periods.length != 1) {
+          if (periods.length !== 1) {
             return false
           }
           return true
@@ -69,35 +69,35 @@ module.exports = function(grunt) {
         css.walk(function(node) {
           switch (node.type) {
             case "comment":
-                if (node.toString().indexOf("/* line") == 0) {
-                  section = /\/(\w+)\.scss/g.exec(node.toString())[1]
-                } else {
-                  comment = node.toString()
-                }
-              break;
+              if (node.toString().indexOf("/* line") === 0) {
+                section = /\/(\w+)\.scss/g.exec(node.toString())[1]
+              } else {
+                comment = node.toString()
+              }
+              break
             case "rule":
-                node.selector.split(",").forEach(function(selector) {
-                  if (trackable(selector) == true) {
-                    compIndex[cleanSelector(selector)] = true
-                    completions.push(buildCompleter(selector, comment, filename, section));
-                    section = null
-                    comment = null
-                  }
-                })
-              break;
+              node.selector.split(",").forEach(function(selector) {
+                if (trackable(selector) === true) {
+                  compIndex[cleanSelector(selector)] = true
+                  completions.push(buildCompleter(selector, comment, filename, section))
+                  section = null
+                  comment = null
+                }
+              })
+              break
           }
-        });
-        fs.writeFileSync("lib/css/data/" + filename + ".js", "module.exports = " + JSON.stringify(completions));
+        })
+        fs.writeFileSync("lib/css/data/" + filename + ".js", "module.exports = " + JSON.stringify(completions))
       }
-    });
+    })
 
     files.forEach(function(file) {
-      var data = fs.readFileSync(file);
+      var data = fs.readFileSync(file)
       postcss([readClasses]).process(data.toString(), {
         syntax: syntax,
         file: file
-      }).then(function (result) {
-      });
-    });
-  });
-};
+      }).then(function() {
+      })
+    })
+  })
+}
